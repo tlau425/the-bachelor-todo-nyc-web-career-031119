9b7054abd965a24f5939eea4e241d9281ceb53a3
Tommy Lau <tlau425@yahoo.com>
2019-03-08 23:30:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,14 +35,17 @@ end
 
 def get_occupation(data, hometown)
   # code here
-  data.each do |season, season_array|
-    season_array.each do |into_array|
-      if into_array[:hometown] == hometown
-        return into_array[:name]
-      end 
-    end 
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
   end
-end
+
+ end	end
 
 def get_average_age_for_season(data, season)
   # code here
