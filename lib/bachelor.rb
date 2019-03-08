ead3d4981a0c83ca654e5eed1c28bad50629622d
Tommy Lau <tlau425@yahoo.com>
2019-03-08 23:11:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,15 @@
 def get_first_name_of_season_winner(data, season)
   # code here
   data[season.to_sym].each do |into_array|
-   if into_array[:status] =="Winner"
-     return into_array[:name].split(" ")[0]
-   end
- end
+    data[season].each do |people|
+
+       people.each do |k,v|
+        if v == "Winner"
+            full_name = people["name"]
+            return full_name.split(' ').first
+        end 
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
